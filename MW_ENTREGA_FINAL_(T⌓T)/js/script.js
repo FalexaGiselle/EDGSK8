72176f05bb86635392224f16c65673be40277f5a
Falexa <121182921+FalexaGiselle@users.noreply.github.com>
2023-01-02 15:02:37 +0100

    Aquí yace el landing page de la web

---
+++ MW_ENTREGA_FINAL_(T⌓T)/js/script.js
@@ -0,0 +1,6 @@
+// M: Miri
+// L: Lely
+// B: White
+// P: Pooh
+// Q&A: Adri 
+// L: Cada vez que SE haga una sección escribe un comentario que ponga al principio "inicio seccion X" y al final "inicio seccion X"
