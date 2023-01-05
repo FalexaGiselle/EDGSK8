f0616cca88b25c2b44fa4d584f5d794612603cbe
Falexa <121182921+FalexaGiselle@users.noreply.github.com>
2023-01-05 23:36:11 +0100

    Página principal pt.1  (falta responsive)

--- MW_ENTREGA_FINAL_(T⌓T)/js/script.js
+++ MW_ENTREGA_FINAL_(T⌓T)/js/script.js
@@ -1,6 +1,24 @@
 // M: Miri
 // L: Lely
-// B: White
+// B: Snowhite
 // P: Pooh
 // Q&A: Adri 
+
+
 // L: Cada vez que SE haga una sección escribe un comentario que ponga al principio "inicio seccion X" y al final "inicio seccion X"
+
+// M: Menú desplegable
+function openMenu() {
+    "use strict";
+    document.getElementById("menu").style.left = "0";
+}
+
+function closeMenu() {
+    "use strict";
+    document.getElementById("menu").style.left = "-100%";
+};
+
+
+// M: Menú desplegable
+// SLIDE
+
